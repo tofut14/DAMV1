006c21c91277e91fd2ad2cb9347f29a12af05053
Vespertino <Vespertino@C7PC-5>
2018-04-27 21:28:04 +0200

    mENSAJE

--- hellogit/src/hellogit/hellogit.java
+++ hellogit/src/hellogit/hellogit.java
@@ -4,14 +4,7 @@ public class hellogit {
 	public static void main(String[] args) {
 		System.out.println("hellogit");
 		System.out.println("hellogit2");
-		//asdsa
-		// Aqui estuvo Carlos..
-		// 18-04-2018.. Aqui estuve
 		
-		// Javier..
-		
-		//Carlos está trabajando en la nueva rama.
-		// UNION ENTRE MASTER Y RAMA_CARLOS
-		// ESTO ES UNA PRUEBA - DE AHORA MISMO!!!!
+		// Hola mundo CRUEL!!!!!!!!!!!!!
 	}
 }
